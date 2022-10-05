a8777d86cd05e3290388d5d2234c5dac11d9847c
italijancic <italijancic@outlook.com>
2022-10-05 09:41:39 -0300

    Add item detail as a child component of ItemDetailContainer

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,27 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
-import {
-  Box,
-  Container,
-  Stack,
-  Text,
-  Image,
-  Flex,
-  VStack,
-  Button,
-  Heading,
-  SimpleGrid,
-  StackDivider,
-  useColorModeValue,
-  Spinner,
-} from '@chakra-ui/react';
-import { MdLocalShipping } from 'react-icons/md';
-
 // Read params from url
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 // My imports
 import { getProduct } from '../../mockAPI/mockAPI'
+import ItemDetail from '../ItemDetail/ItemDetail';
 
 export default function ItemDetailContainer() {
 
@@ -38,108 +22,6 @@ export default function ItemDetailContainer() {
   }, [id])
 
   return (
-    <Container maxW={'7xl'}>
-      <SimpleGrid
-        columns={{ base: 1, lg: 2 }}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 18, md: 24 }}>
-        <Flex>
-          {product.img ?
-            <Image
-              rounded={'md'}
-              alt={'product image'}
-              src={product.img}
-              fit={'cover'}
-              align={'center'}
-              w={'100%'}
-              h={{ base: '100%', sm: '400px', lg: '500px' }}
-            />
-            :
-            <Spinner />
-          }
-        </Flex>
-        <Stack spacing={{ base: 6, md: 10 }}>
-          <Box as={'header'}>
-            <Heading
-              lineHeight={1.1}
-              fontWeight={600}
-              fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
-              {product.title}
-            </Heading>
-            <Text
-              color={useColorModeValue('gray.900', 'gray.400')}
-              fontWeight={300}
-              fontSize={'2xl'}>
-
-              {product.price ? `U$S ${product.price}` : ''}
-            </Text>
-          </Box>
-
-          <Stack
-            spacing={{ base: 4, sm: 6 }}
-            direction={'column'}
-            divider={
-              <StackDivider
-                borderColor={useColorModeValue('gray.200', 'gray.600')}
-              />
-            }>
-            <VStack spacing={{ base: 4, sm: 6 }}>
-              <Text
-                color={useColorModeValue('gray.500', 'gray.400')}
-                fontSize={'2xl'}
-                fontWeight={'300'}>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod tempor invidunt ut labore
-              </Text>
-              <Text fontSize={'lg'}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
-                aliquid amet at delectus doloribus dolorum expedita hic, ipsum
-                maxime modi nam officiis porro, quae, quisquam quos
-                reprehenderit velit? Natus, totam.
-              </Text>
-            </VStack>
-          </Stack>
-
-          <Link to={'/'}>
-            <Button
-              rounded={'none'}
-              w={'full'}
-              mt={8}
-              size={'lg'}
-              py={'7'}
-              bg={useColorModeValue('gray.900', 'gray.50')}
-              color={useColorModeValue('white', 'gray.900')}
-              textTransform={'uppercase'}
-              _hover={{
-                transform: 'translateY(2px)',
-                boxShadow: 'lg',
-              }}>
-              Volver
-            </Button>
-          </Link>
-
-          {/* <Button
-            rounded={'none'}
-            w={'full'}
-            mt={8}
-            size={'lg'}
-            py={'7'}
-            bg={useColorModeValue('gray.900', 'gray.50')}
-            color={useColorModeValue('white', 'gray.900')}
-            textTransform={'uppercase'}
-            _hover={{
-              transform: 'translateY(2px)',
-              boxShadow: 'lg',
-            }}>
-            Comprar
-          </Button> */}
-
-          <Stack direction="row" alignItems="center" justifyContent={'center'}>
-            <MdLocalShipping />
-            <Text color="green.500">Entrega en 2-3 días hábiles</Text>
-          </Stack>
-        </Stack>
-      </SimpleGrid>
-    </Container>
+    <ItemDetail product={product}/>
   );
 }
